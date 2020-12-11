210ea05f53a8a91cedfab1de8595ee38770aa29c
midesweb <malvarez@desarrolloweb.com>
2020-12-11 18:10:03 +0100

    clase 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+      width: {
+        '420': '420px',
+      },
+      minWidth: {
+        '300': '300px',
+        '400': '400px',
+      },
+      gridTemplateColumns: {
+        'main_md': '1fr 300px',
+        'main_lg': '200px 1fr 300px'
+      },
+      colors: {
+        primary: '#efc',
+        secondary: '#666',
+      },
+      screens: {
+        'xs': '420px',
+      }
+    },
   },
   variants: {
     extend: {
