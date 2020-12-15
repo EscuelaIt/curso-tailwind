07c71d56af669d1ae5d20e1dc7a35ffdd6ff30a4
midesweb <malvarez@desarrolloweb.com>
2020-12-15 11:23:15 +0100

    modificaciones finales clase 04

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [
     './**/*.html',
+    './**/*.blade.php',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      boxShadow: {
+        roja: '0 4px 6px -1px rgba(200, 0, 0, 0.1), 0 2px 4px -1px rgba(200, 0, 0, 0.06)'
+      },
       spacing: {
         '420': '420px',
       },
@@ -19,7 +23,11 @@ module.exports = {
         'main_lg': '200px 1fr 300px'
       },
       colors: {
-        primary: '#efc',
+        primary: { 
+          DEFAULT: '#efc',
+          '100': '#fdd',
+          '200': 'red',
+        },
         secondary: '#666',
         orange: colors.orange,
       },
